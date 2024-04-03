254cf4e02fae3426b08fd6065a77c0a9a7e728b7
Sun Young Ryu <syryu@rcnp.osaka-u.ac.jp>
2024-04-03 21:22:03 +0900

    Add new data structure for AmQ

--- AmQStrTdcData.h
+++ AmQStrTdcData.h
@@ -9,6 +9,8 @@ namespace AmQStrTdc::Data {
     uint8_t d[8];
   };
 
+  inline namespace v0 {
+
   struct Bits {
     union {
       uint8_t d[8];
@@ -70,7 +72,78 @@ namespace AmQStrTdc::Data {
     SpillEnd      = 0x14
   };
 
-} // nemacspace AmQStrTdc::Data
+  } // namespace v0
+
+
+  namespace v1 {
+
+  struct Bits {
+    union {
+      uint8_t d[8];
+
+      struct {
+	uint64_t raw : 64;
+      };
+
+      // common
+      struct {
+	uint64_t com_rsv : 58; // [57:0]
+	uint64_t head    :  6; // [63:58]
+      };
+   
+      // lrtdc
+      struct {
+	uint64_t zero_t1    : 16; // [15:0]	  	  
+	uint64_t tdc        : 19; // [34:16]
+	uint64_t tot        : 16; // [50:35]
+	uint64_t ch         :  7; // [57:51]
+	uint64_t dtype      :  6; // [63:58]
+      };
+
+      // hrtdc
+      struct {
+	uint64_t hrtdc      : 29; // [28:0]
+	uint64_t hrtot      : 22; // [50:29]
+	uint64_t hrch       :  7; // [57:51]
+	uint64_t hrdtype    :  6; // [63:58]
+      };
+
+      // 1st heartbeat
+      struct {
+	uint64_t zero_h     : 24;
+	uint64_t toffset    : 16; // [39,24]
+	uint64_t hbflag     : 16; // [55,40]
+	uint64_t reserve1   : 2;  // [57,56]
+	uint64_t hbtype1    : 6;  // [63,58]
+      };
+
+      // 2nd heartbeat
+      struct {
+	uint64_t transSize : 20;	  
+	uint64_t geneSize  : 20; // [39,20]
+	uint64_t userReg   : 16; // [55,40]
+	uint64_t reserve2  :  2; // [57,56]
+	uint64_t hbtype2   :  6; // [63,58]
+      };
+
+    };
+  };
+
+  enum HeadTypes {
+    Data              = 0x0B,
+    Heartbeat1st      = 0x1C,
+    Heartbeat2nd      = 0x1E,    
+    Trailer           = 0x0D,
+    ThrottlingT1Start = 0x19,
+    ThrottlingT1End   = 0x11,
+    ThrottlingT2Start = 0x1A,
+    ThrottlingT2End   = 0x12,        
+  };
+
+  } // namespace v1
+  
+  
+} // namespace AmQStrTdc::Data
 
 
 
