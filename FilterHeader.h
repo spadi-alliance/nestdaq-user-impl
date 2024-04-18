de5b28992171b67096f3580815a600e2c5574a15
IGARASHI Youichi <youichi.igarashi@kek.jp>
2024-04-19 01:43:39 +0900

    add new header files

--- FilterHeader.h
+++ FilterHeader.h
@@ -4,10 +4,9 @@
 #include <cstdint>
 #include <sys/time.h>
 
-#pragma pack(4)
 namespace Filter {
 
-inline namespace v0 {
+namespace v0 {
 // "NIOC-TLF" : little endian of "FLT-COIN"
 constexpr uint64_t MAGIC {0x4e494f43'2d544c46};
 
@@ -19,25 +18,56 @@ struct Header {
     uint32_t elapseTime  {0};
     struct timeval processTime {0, 0};
 };
-} //v0
+} //namespace v0
 
-namespace v1 {
+inline namespace v1 {
+#pragma pack(4)
+
 // " IGOLTLF" : little endian of "FLTLOGI "
 constexpr uint64_t MAGIC {0x0049474f'4c544c46};
 
 struct Header {
     uint64_t magic       {MAGIC};
     uint32_t length      {0};
-    uint16_t hLength     {20};
+    uint16_t hLength     {36};
     uint16_t type        {0};
+    uint32_t timeFrameId {0};
     uint32_t numTrigs    {0};
     uint32_t workerId    {0};
+    uint32_t numMessages {0};
     uint32_t elapseTime  {0};
     struct timeval processTime {0, 0};
 };
+
+// " EMITGRT"
+constexpr uint64_t TDC_MAGIC {0x00454d49'54475254};
+struct TrgTimeHeader {
+    union {
+        uint32_t u32data[4];
+        struct {
+            uint64_t magic       {TDC_MAGIC};
+            uint32_t length      {0};
+            uint16_t hLength     {0x14};
+            uint16_t type        {0};
+            //uint32_t timeFrameId {0};
+        };
+    };
+};
+
+constexpr unsigned char FLT_TDC_TYPE {0xaa};
+struct TrgTime {
+    union {
+        unsigned char cdata[4]  {0, 0, 0, FLT_TDC_TYPE};
+        struct {
+            uint32_t time : 24;
+            uint32_t type :  8;
+        };
+    };
+};
+
+#pragma pack()
 } //namespace v1
 
 } // namespace Filter
-#pragma pack()
 
 #endif
---
+++ HartbeatFrameHeader.h
@@ -0,0 +1,18 @@
+#ifndef HBFHeader_H
+#define HBFHeader_H
+
+namespace HartbeatFrame
+{
+// "HRTBEAT\0" -> "\0TAEBTRH"
+constexpr uint64_t MAGIC = 0x0054414542545248;
+#pragma pack(4)
+struct Header {
+    uint64_t magic      {MAGIC};
+    uint32_t length     {0};
+    uint16_t hLength    {16};
+    uint16_t type       {0};
+};
+#pragma pack()
+}
+
+#endif
