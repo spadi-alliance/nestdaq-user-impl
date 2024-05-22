78673ed66e5686708b574e52162d3815273c0c34
Shinsuke OTA <ota@rcnp.osaka-u.ac.jp>
2024-05-23 07:22:17 +0900

    [high level filter] divide Filter...ABC source into two parts to fix conflict of main function

--- FilterTimeFrameSliceBySomething.cxx
+++ FilterTimeFrameSliceBySomething.cxx
@@ -2,12 +2,13 @@
  * @file FilterTimeFrameSliceBySomething
  * @brief Slice Timeframe by Logic timing for NestDAQ
  * @date Created : 2024-05-04 12:31:55 JST
- *       Last Modified : 2024-05-21 17:30:38 JST
+ *       Last Modified : 2024-05-23 07:12:22 JST
  *
  * @author Shinsuke OTA <ota@rcnp.osaka-u.ac.jp>
  *
  */
 #include "FilterTimeFrameSliceBySomething.h"
+#include "FilterTimeFrameSliceABC.icxx"
 #include "fairmq/runDevice.h"
 
 #include "utility/MessageUtil.h"
@@ -19,12 +20,15 @@ using nestdaq::FilterTimeFrameSliceBySomething;
 namespace bpo = boost::program_options;
 
 
+
+
 FilterTimeFrameSliceBySomething::FilterTimeFrameSliceBySomething()
 {
 }
 
-bool FilterTimeFrameSliceBySomething::ProcessSlice(TTF& tf) {
-
+bool FilterTimeFrameSliceBySomething::ProcessSlice(TTF& tf)
+{
+#if 0
    int doKeep = false;
 
    if (tf[0]->GetHeader()->femid == /* some module */) {
@@ -42,7 +46,7 @@ bool FilterTimeFrameSliceBySomething::ProcessSlice(TTF& tf) {
    if (!doKeep) {
       return false;
    }
-   
+#endif   
    return true;
 }
 
@@ -55,8 +59,27 @@ bool FilterTimeFrameSliceBySomething::ProcessSlice(TTF& tf) {
 void addCustomOptions(bpo::options_description& options)
 {
    using opt = FilterTimeFrameSliceBySomething::OptionKey;
-// option can be defined   
+
+   options.add_options()
+      (opt::InputChannelName.data(),
+       bpo::value<std::string>()->default_value("in"),
+       "Name of the input channel")
+      (opt::OutputChannelName.data(),
+       bpo::value<std::string>()->default_value("out"),
+       "Name of the output channel")
+      (opt::DQMChannelName.data(),
+       bpo::value<std::string>()->default_value("dqm"),
+       "Name of the data quality monitoring channel")
+      (opt::PollTimeout.data(),
+       bpo::value<std::string>()->default_value("1"),
+       "Timeout of polling (in msec)")
+      (opt::SplitMethod.data(),
+       bpo::value<std::string>()->default_value("1"),
+       "STF split method")
+      ;
+   
 }
+
 
 
 std::unique_ptr<fair::mq::Device> getDevice(fair::mq::ProgOptions& /*config*/)
--- FilterTimeFrameSliceBySomething.h
+++ FilterTimeFrameSliceBySomething.h
@@ -2,7 +2,7 @@
  * @file FilterTimeFrameSliceBySomething.h
  * @brief Abstract base class for filtering time frame slice
  * @date Created : 2024-05-04 12:27:57 JST
- *       Last Modified : 2024-05-21 17:37:01 JST
+ *       Last Modified : 2024-05-23 07:16:28 JST
  *
  * @author Shinsuke OTA <ota@rcnp.osaka-u.ac.jp>
  *
@@ -19,13 +19,14 @@
 #include "FilterHeader.h"
 #include "HeartbeatFrameHeader.h"
 #include "FrameContainer.h"
+#include "FilterTimeFrameSliceABC.h"
 
 namespace nestdaq {
    class FilterTimeFrameSliceBySomething;
 }
 
 
-class nestdaq::FilterTimeFrameSliceBySomething : public FilterTimeFrameSliceABC {
+class nestdaq::FilterTimeFrameSliceBySomething : public nestdaq::FilterTimeFrameSliceABC {
 public:
    FilterTimeFrameSliceBySomething();
    virtual ~FilterTimeFrameSliceBySomething() override = default;
